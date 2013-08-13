688dce099b1e0209b90c22f0eb7a16cfe387502a
Zhenyi <zeewhy@gmail.com>
2013-08-13 02:12:13 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hacktrg::Application.config.secret_key_base = '615334891cae81ef982e6a37070eaee239af3d58decf20cce55f8f5561bd647d1256bbd3959587c9903b3e7ea8ca80563777a62d5148e096395aa2cc7e338ec1'
